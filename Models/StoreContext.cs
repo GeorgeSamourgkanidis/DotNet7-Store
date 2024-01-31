932572b4f3547d8e2ffc210bc810c73a1f26661e
George Samourgkanidis <giorgossamou1@gmail.com>
2024-01-31 14:23:42 +0200

    Initialize dotnet7 with entityFramework and localDb connection

---
+++ Models/StoreContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_DotNet7_products.Models;
+
+public partial class StoreContext : DbContext
+{
+    public StoreContext()
+    {
+    }
+
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(LocalDb)\\LocalProducts;Database=Store;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
